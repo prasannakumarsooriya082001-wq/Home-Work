fab76f25ac77a358bc3b2c9994077a04ca7ad784
Prasanna Kumar <prasannakumarsooriya082001@gmail.com>
2026-02-11 16:00:46 +0530

    day12 loop practice

---
+++ day12 loop practice/Fatorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Fatorial
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the Number : ");
+		int num = sc.nextInt();
+		
+		int fact = 1 ;
+		
+		for(int i = 1 ; i <= num ; i++)
+		{
+			fact *= i;
+		}	
+			
+		System.out.println(fact);	
+
+		sc.close();
+	}
+}
